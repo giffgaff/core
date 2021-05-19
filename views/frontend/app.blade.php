b0b773e2da9bb0306c2a10f32d69ef423e710866
tariqwalji <tariqwalji@yahoo.co.uk>
2021-05-19 02:20:21 +0100

    COM-739 Transpiles using latest flarum webpack

--- views/frontend/app.blade.php
+++ views/frontend/app.blade.php
@@ -15,6 +15,7 @@
         <div id="alerts"></div>
 
         <!-- workaround for old iOS devices -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.6/es6-shim.min.js" integrity="sha512-Dg4/NsM35WYe4Vpj/ZzxsN7K4ifsi6ecw9+VB5rmCntqoQjEu4dQxL6/qQVebHalidCqWiVkWVK59QtJCCjBDQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/es7-shim/6.0.0/es7-shim.min.js" integrity="sha512-LUBx8YucSzBQpB8HbxpcI07wpM/IN3BRfTshhgWh/X/Bpl9jhLgHjUyShOdCP+yLgBZMRdsEOERXJilznrP1tg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/proxy-polyfill/0.3.2/proxy.min.js" integrity="sha512-rkQmC9k6r2JmC6vL68NnR2dPJnAg2zcAX562Y097B2TVaxUkF910WNhHJgdgz+NX2bJ0Lx8RegJ1vE87OKWlag==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
